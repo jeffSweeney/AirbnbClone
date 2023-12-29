4ee95d9198b46791773e1a79b2ba8d06d3a9ac9b
Jeff Sweeney <Jeffrey.Sweeney812.GH@gmail.com>
2023-12-29 10:36:39 -0500

    Initialize skeleton app

---
+++ AirbnbClone/AirbnbClone/AirbnbCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbCloneApp.swift
+//  AirbnbClone
+//
+//  Created by Jeffrey Sweeney on 12/29/23.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
