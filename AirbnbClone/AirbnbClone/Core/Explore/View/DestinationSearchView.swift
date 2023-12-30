fbc158330084972b639c61460fb55a99e496b1f3
Jeff Sweeney <Jeffrey.Sweeney812.GH@gmail.com>
2023-12-30 13:38:06 -0500

    Complete DestinationSearchView

--- AirbnbClone/AirbnbClone/Core/Explore/View/DestinationSearchView.swift
+++ AirbnbClone/AirbnbClone/Core/Explore/View/DestinationSearchView.swift
@@ -17,18 +17,51 @@ struct DestinationSearchView: View {
     @Binding var showSelf: Bool
     @State private var destination = ""
     @State private var currentDestinationOption: DestinationSearchOptions = .location
+    @State private var fromDate = Date()
+    @State private var toDate = Date().addingTimeInterval(86400)
+    @State private var numGuests = 1
+    
+    private var earliestCheckout: Date {
+        return fromDate.addingTimeInterval(86400)
+    }
+    
+    private var guestCountLabel: String {
+        let text = numGuests > 1 ? "Adults" : "Adult"
+        
+        return "\(numGuests) \(text)"
+    }
     
     var body: some View {
         VStack {
-            Button {
-                withAnimation(.snappy) {
-                    showSelf.toggle()
+            HStack {
+                Button {
+                    withAnimation(.snappy) {
+                        showSelf.toggle()
+                    }
+                } label: {
+                    Image(systemName: "xmark.circle")
+                        .imageScale(.large)
+                        .foregroundStyle(.black)
                 }
-            } label: {
-                Image(systemName: "xmark.circle")
-                    .imageScale(.large)
+                
+                Spacer()
+                
+                if !destination.isEmpty {
+                    Button("Clear") {
+                        withAnimation(.snappy) {
+                            destination = ""
+                            withAnimation(.snappy) { currentDestinationOption = .location }
+                            fromDate = Date()
+                            toDate = Date().addingTimeInterval(86400)
+                            numGuests = 1
+                        }
+                    }
                     .foregroundStyle(.black)
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                }
             }
+            .padding()
             
             // MARK: - Where?
             VStack(alignment: .leading) {
@@ -55,54 +88,55 @@ struct DestinationSearchView: View {
                     CollapsedCard(title: "Where", description: "Add destination")
                 }
             }
-            .padding()
-            .frame(height: currentDestinationOption == .location ? 120 : 64)
-            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
-            .background(.white)
-            .clipShape(RoundedRectangle(cornerRadius: 12))
-            .padding()
-            .shadow(radius: 10)
+            .modifier(CollapsibleDestinationViewModifier(height: currentDestinationOption == .location ? 120 : 64))
             .onTapGesture {
                 withAnimation(.snappy) { currentDestinationOption = .location }
             }
             
             // MARK: - When?
-            VStack {
+            VStack(alignment: .leading) {
                 if currentDestinationOption == .dates {
-                    Text("Show expanded view")
+                    Text("When is your trip?")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                    
+                    VStack {
+                        DatePicker("From", selection: $fromDate, in: Date()..., displayedComponents: .date)
+                        
+                        Divider()
+                        
+                        DatePicker("To", selection: $toDate, in: earliestCheckout..., displayedComponents: .date)
+                    }
+                    .foregroundStyle(.gray)
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
                 } else {
                     CollapsedCard(title: "When", description: "Add dates")
                 }
             }
-            .padding()
-            .frame(height: currentDestinationOption == .dates ? 120 : 64)
-            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
-            .background(.white)
-            .clipShape(RoundedRectangle(cornerRadius: 12))
-            .padding()
-            .shadow(radius: 10)
+            .modifier(CollapsibleDestinationViewModifier(height: currentDestinationOption == .dates ? 180 : 64))
             .onTapGesture {
                 withAnimation(.snappy) { currentDestinationOption = .dates }
             }
             
             // MARK: - Who?
-            VStack {
+            VStack(alignment: .leading) {
                 if currentDestinationOption == .guests {
-                    Text("Show expanded view")
+                    Text("Who's coming?")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                    
+                    Stepper(guestCountLabel, value: $numGuests, in: 1 ... Int.max)
                 } else {
                     CollapsedCard(title: "Who", description: "Add guests")
                 }
             }
-            .padding()
-            .frame(height: currentDestinationOption == .guests ? 120 : 64)
-            .frame(maxWidth: .infinity)
-            .background(.white)
-            .clipShape(RoundedRectangle(cornerRadius: 12))
-            .padding()
-            .shadow(radius: 10)
+            .modifier(CollapsibleDestinationViewModifier(height: currentDestinationOption == .guests ? 120 : 64))
             .onTapGesture {
                 withAnimation(.snappy) { currentDestinationOption = .guests }
             }
+            
+            Spacer()
         }
     }
 }
@@ -129,5 +163,20 @@ struct CollapsedCard: View {
             .font(.subheadline)
             .fontWeight(.semibold)
         }
+    }
+}
+
+// MARK: - Custom View Modifiers
+struct CollapsibleDestinationViewModifier: ViewModifier {
+    let height: CGFloat
+    
+    func body(content: Content) -> some View {
+        content
+            .padding()
+            .frame(height: height)
+            .background(.white)
+            .clipShape(RoundedRectangle(cornerRadius: 12))
+            .padding()
+            .shadow(radius: 10)
     }
 }
